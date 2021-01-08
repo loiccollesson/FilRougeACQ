022880983b21c3785a496c28d6083bdaf203687a
skyd6 <75314034+skyd6@users.noreply.github.com>
2021-01-08 16:06:43 +0100

    Update script.js

--- Page Quentin/script.js
+++ Page Quentin/script.js
@@ -1,4 +1,4 @@
-document.querySelector("#bouton").addEventListener("mousedown", function(){
+document.querySelector("#toggleBtn").addEventListener("mousedown", function(){
     console.log("click");
     document.querySelector(".page").classList.toggle("dark");
 })
