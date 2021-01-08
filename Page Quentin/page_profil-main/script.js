63726a22c309a8c73cb5adbd526db0e295c16e6a
skyd6 <75314034+skyd6@users.noreply.github.com>
2021-01-08 21:16:21 +0100

    add files Quentin

---
+++ Page Quentin/page_profil-main/script.js
@@ -0,0 +1,29 @@
+document.querySelector("#bouton").addEventListener("mousedown", function(){
+    console.log("click");
+    document.querySelector(".page").classList.toggle("dark");
+})
+
+document.querySelector("#bouton").addEventListener("mousedown", function(){
+    console.log("click");
+    document.querySelector("body").classList.toggle("newbody");
+})
+
+document.querySelector("#bouton").addEventListener("mousedown", function(){
+    console.log("click");
+    document.querySelector(".bloc1").classList.toggle("newlogo");
+})
+
+document.querySelector("#bouton").addEventListener("mousedown", function(){
+    console.log("click");
+    document.querySelector("nav").classList.toggle("newnav");
+})
+
+document.querySelector("#bouton").addEventListener("mousedown", function(){
+    console.log("click");
+    document.querySelector(".Alb").classList.toggle("newalb");
+})
+
+document.querySelector("#bouton").addEventListener("mousedown", function(){
+    console.log("click");
+    document.querySelector("table").classList.toggle("newtable");
+})
